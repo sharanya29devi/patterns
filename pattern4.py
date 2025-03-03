46c06c3b8fcc5871570a904b59384fab27e22995
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-03 12:00:32 +0530

    Update pattern4.py

--- pattern4.py
+++ pattern4.py
@@ -1,5 +1,5 @@
 n = 4
-a = ""
-for i in range(1, n+1):
-    a += "*"*i + " "
-print(a)
+for i in range(1, N+1):
+            for j in range(1, i+1):
+                print("*", end="")
+            print("", end=" ")
