273da0c5700d63e6ef8904a5207b4005fbd38918
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-03 12:18:48 +0530

    Update pattern5.py

--- pattern5.py
+++ pattern5.py
@@ -1,7 +1,6 @@
 n = 4
 
-for i in range(1,n+1):
-    a =""
-    for j in range(1,n-i+2):
-        a += str(j)
-    print(a)
+for i in range(1, n+4):
+    for j in range(1, n-i+2):
+        print(j, end="")
+    print("")
